9862827105640e45479d6b1f1fab260472db1ef1
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-03 00:37:06 +0100

    added prisma service to auth module

--- user/src/auth/auth.module.ts
+++ user/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
   imports: [
     JwtModule.register({
       global: true,
