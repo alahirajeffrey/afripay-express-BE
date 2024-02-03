2991858f98d6a685bcbf6e2b3b5fdf09ac5c231b
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-03 09:36:45 +0100

    commit

--- user/src/auth/guards/authentication.guard.ts
+++ user/src/auth/guards/authentication.guard.ts
@@ -8,7 +8,7 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { jwtConstants } from '../constants';
 import { Reflector } from '@nestjs/core';
-// import { IS_PUBLIC_KEY } from 'src/auth/decorators/public.decorator';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
@@ -18,14 +18,14 @@ export class JwtGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-    //   context.getHandler(),
-    //   context.getClass(),
-    // ]);
-    // if (isPublic) {
-    //   // 💡 See this condition
-    //   return true;
-    // }
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      // 💡 See this condition
+      return true;
+    }
 
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
