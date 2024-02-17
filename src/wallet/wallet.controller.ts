f51597fee36e740757016d4e719c03a3ba252de2
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-17 21:36:44 +0100

    controller to create wallet

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { WalletService } from './wallet.service';
 
+@ApiTags('wallet-endpoints')
 @Controller('wallet')
-export class WalletController {}
+export class WalletController {
+  constructor(private walletService: WalletService) {}
+
+  @Post('create')
+  @ApiOperation({ summary: 'Create wallet and virtual account' })
+  registerAccount(@Req() req) {
+    return this.walletService.createWallet(req.user.email);
+  }
+}
