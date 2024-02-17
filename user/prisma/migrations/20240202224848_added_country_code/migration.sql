41dd58dec108d5a51c27a241cfe96040fa0e9a89
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-17 19:24:28 +0100

    commit

--- user/prisma/migrations/20240202224848_added_country_code/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Account" ADD COLUMN     "countryCode" TEXT DEFAULT '234';
--- user/prisma/migrations/20240202232443_added_kyc_columns/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Account" ADD COLUMN     "bvn" TEXT,
-ADD COLUMN     "nationalIdCardUrl" TEXT,
-ADD COLUMN     "profilePicture" TEXT;
