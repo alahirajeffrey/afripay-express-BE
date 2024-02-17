8ce488deee53e877ceda5b7730e075f0b3f4961f
alahirajeffrey <alahirajeffrey@gmail.com>
2024-02-17 21:37:01 +0100

    setup config and util service

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -4,11 +4,15 @@ import { WalletController } from './wallet.controller';
 import { PrismaService } from 'src/prisma.service';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from 'src/auth/guards/authentication.guard';
+import { ConfigService } from '@nestjs/config';
+import { UtilService } from 'src/utils/utils.utils';
 
 @Module({
   providers: [
     WalletService,
     PrismaService,
+    ConfigService,
+    UtilService,
     { provide: APP_GUARD, useClass: JwtGuard },
   ],
   controllers: [WalletController],
